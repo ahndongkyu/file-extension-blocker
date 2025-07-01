91441f19530ac3c6c0cf6421985713b249ec9b34
“dongkyu” <kjuvcz95@gamil.com>
2025-07-01 19:07:23 +0900

    folder_structure&react_setting

--- routes/extensionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/extensionController');
-
-router.get('/', controller.getAllExtensions);
-router.post('/custom', controller.addCustomExtension);
-
-module.exports = router;
